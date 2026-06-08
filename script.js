b8c2455cfdbf49d9e8366fd0aaa57ec57ae07a32
pbLola <lohannecastro88@gmail.com>
2026-06-08 10:48:22 -0300

    feat(calculadora): adicionar lógica principal no arquivo JavaScript

---
+++ script.js
@@ -0,0 +1,13 @@
+function adicionarCaracter (caracter){
+    const valorDisplay = document.querySelector('.display').value
+    document.querySelector('.display').value = valorDisplay + caracter
+
+    console.log(valorDisplay)
+}
+function limparTela(){
+    document.querySelector('.display'). value = ''
+}
+function calcular(){
+    const valorDisplay = document.querySelector('.display').value 
+    document.querySelector('.display').value = eval(valorDisplay)
+}
